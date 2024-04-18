3b66b1d143d9f4bfce6320f040b9aa4b98f69335
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-04-18 13:35:51 +0200

    instantiate two Production objects

--- index.php
+++ index.php
@@ -10,6 +10,15 @@ class Production
     {
         $this->title = $title;
         $this->language = $language;
-        $this->rating = $rating;
+        if ($rating >= 1 && $rating <= 10) {
+            $this->rating = $rating;
+        } else {
+            throw new InvalidArgumentException("Rating must be between 1 and 10");
+        }
     }
 }
+
+$production1 = new Production('Matrix', 'English', 9);
+$production2 = new Production('Parasite', 'Korean', 10);
+
+var_dump($production1, $production2);
