226134ba3b7e0a744c44893276278da7707f8bb5
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-04-18 13:21:13 +0200

    add class Production

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+class Production
+{
+    public $title;
+    public $language;
+    public $rating;
+
+    public function __construct($title, $language, $rating)
+    {
+        $this->title = $title;
+        $this->language = $language;
+        $this->rating = $rating;
+    }
+}
